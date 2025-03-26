faeab4437292e8130f43ec0aa2a9f65520a1aaad
XdotCore <xandercorvalan@gmail.com>
2025-03-26 01:13:54 -0500

    use oncelock pattern for static

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,19 @@
 use ctor::ctor;
 use dlopen::raw::Library;
+use std::sync::OnceLock;
 
 #[cfg(windows)]
 pub mod windows;
 
 #[ctor]
 fn hello() {
-    unsafe {
-        static mut BLUEBRICK: Option<Library> = None;
+    static BLUEBRICK: OnceLock<Option<Library>> = OnceLock::new();
 
-        BLUEBRICK = match Library::open("bluebrick/bluebrick.dll") {
-            Ok(lib) => Some(lib),
-            Err(e) => {
-                msgbox::create("Error Loading BlueBrick", &format!("Problem opening loader:\n{e:?}"), msgbox::IconType::Error);
-                return;
-            }
-        };
-    }
+    BLUEBRICK.get_or_init(|| match Library::open("bluebrick/bluebrick.dll") {
+        Ok(lib) => Some(lib),
+        Err(e) => {
+            msgbox::create("Error Loading BlueBrick", &format!("Problem opening loader:\n{e:?}"), msgbox::IconType::Error);
+            None
+        }
+    });
 }
