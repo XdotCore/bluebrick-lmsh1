7726d150d73df5b4186ec02900c3e5587091103b
XdotCore <xandercorvalan@gmail.com>
2025-04-03 22:09:55 -0500

    add debug message for debugging

--- proxy/src/lib.rs
+++ proxy/src/lib.rs
@@ -6,6 +6,8 @@ pub mod windows;
 
 #[ctor]
 fn hello() {
+    let _ = msgbox::create("For debugging", "For debugging", msgbox::IconType::None);
+
     load_bluebrick(Config {
         platform: RequestedPlatform::Win32,
         renderer: RequestedRenderer::DX9
