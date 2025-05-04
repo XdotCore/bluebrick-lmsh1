708f48a9f2d5613815194b9b0169a73c5cf29e2d
XdotCore <xdotcoredev@gmail.com>
2025-05-04 13:01:47 -0500

    fix by remove old definitions

--- proxy/src/windows.rs
+++ proxy/src/windows.rs
@@ -3,13 +3,6 @@ use dlopen_derive::WrapperApi;
 use windows::Win32::UI::Input::XboxController::{XINPUT_CAPABILITIES, XINPUT_FLAG, XINPUT_STATE, XINPUT_VIBRATION};
 use std::sync::OnceLock;
 
-#[link(name = "XInput1_3", kind = "raw-dylib")]
-unsafe extern "C" {
-    pub unsafe fn XInputGetState(wuserindex: u32, pstate: *mut XINPUT_STATE) -> u32;
-    pub unsafe fn XInputSetState(dwuserindex: u32, pvibration: *const XINPUT_VIBRATION) -> u32;
-    pub unsafe fn XInputGetCapabilities(dwuserindex: u32, dwflags: XINPUT_FLAG, pcapabilities: *mut XINPUT_CAPABILITIES) -> u32;
-}
-
 #[derive(WrapperApi)]
 struct XInput1_3API {
     XInputGetState: extern "C" fn(wuserindex: u32, pstate: *mut XINPUT_STATE) -> u32,
