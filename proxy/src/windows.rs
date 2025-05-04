20212b05aa9a2b7d55c5aa24de2d24b009e838c4
XdotCore <xdotcoredev@gmail.com>
2025-05-04 12:57:39 -0500

    test loading proxy at runtime

--- proxy/src/windows.rs
+++ proxy/src/windows.rs
@@ -1,8 +1,46 @@
+use dlopen::wrapper::{Container, WrapperApi};
+use dlopen_derive::WrapperApi;
 use windows::Win32::UI::Input::XboxController::{XINPUT_CAPABILITIES, XINPUT_FLAG, XINPUT_STATE, XINPUT_VIBRATION};
+use std::sync::OnceLock;
 
 #[link(name = "XInput1_3", kind = "raw-dylib")]
 unsafe extern "C" {
     pub unsafe fn XInputGetState(wuserindex: u32, pstate: *mut XINPUT_STATE) -> u32;
     pub unsafe fn XInputSetState(dwuserindex: u32, pvibration: *const XINPUT_VIBRATION) -> u32;
     pub unsafe fn XInputGetCapabilities(dwuserindex: u32, dwflags: XINPUT_FLAG, pcapabilities: *mut XINPUT_CAPABILITIES) -> u32;
+}
+
+#[derive(WrapperApi)]
+struct XInput1_3API {
+    XInputGetState: extern "C" fn(wuserindex: u32, pstate: *mut XINPUT_STATE) -> u32,
+    XInputSetState: extern "C" fn(dwuserindex: u32, pvibration: *const XINPUT_VIBRATION) -> u32,
+    XInputGetCapabilities: extern "C" fn(dwuserindex: u32, dwflags: XINPUT_FLAG, pcapabilities: *mut XINPUT_CAPABILITIES) -> u32,
+}
+
+fn xinput() -> Container<XInput1_3API> {
+    let once = OnceLock::<Container<XInput1_3API>>::new();
+    once.get_or_init(|| {
+        match unsafe { Container::<BBApi>::load("XInput1_3") } {
+            Ok(xinput) => xinput,
+            Err(e) => {
+                let _ = msgbox::create("Error Loading BlueBrick", &format!("Problem opening original for proxy (XInput1_3.dll):\n{e:?}"), msgbox::IconType::Error);
+                return;
+            }
+        };
+    })
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern fn XInputGetState(wuserindex: u32, pstate: *mut XINPUT_STATE) -> u32 {
+    xinput().XInputGetState(wuserindex, pstate)
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern fn XInputSetState(dwuserindex: u32, pvibration: *const XINPUT_VIBRATION) -> u32 {
+    xinput().XInputSetState(dwuserindex, pvibration)
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern fn XInputGetCapabilities(dwuserindex: u32, dwflags: XINPUT_FLAG, pcapabilities: *mut XINPUT_CAPABILITIES) -> u32 {
+    xinput().XInputGetCapabilities(dwuserindex, dwflags, pcapabilities)
 }
