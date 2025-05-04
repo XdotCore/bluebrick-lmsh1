fad6437a084ad075191ac4b40016891bbee7cd0e
XdotCore <xdotcoredev@gmail.com>
2025-05-04 13:18:06 -0500

    fix type errors and warnings

--- proxy/src/windows.rs
+++ proxy/src/windows.rs
@@ -18,24 +18,25 @@ fn xinput() -> Container<XInput1_3API> {
         match unsafe { Container::<XInput1_3API>::load("XInput1_3") } {
             Ok(xinput) => xinput,
             Err(e) => {
-                let _ = msgbox::create("Error Loading BlueBrick", &format!("Problem opening original for proxy (XInput1_3.dll):\n{e:?}"), msgbox::IconType::Error);
-                return;
+                let e = format!("Problem opening original for proxy (XInput1_3.dll):\n{e:?}");
+                let _ = msgbox::create("Error Loading BlueBrick", &e, msgbox::IconType::Error);
+                panic!("{e}")
             }
         }
     })
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern fn XInputGetState(wuserindex: u32, pstate: *mut XINPUT_STATE) -> u32 {
+pub extern "C" fn XInputGetState(wuserindex: u32, pstate: *mut XINPUT_STATE) -> u32 {
     xinput().XInputGetState(wuserindex, pstate)
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern fn XInputSetState(dwuserindex: u32, pvibration: *const XINPUT_VIBRATION) -> u32 {
+pub extern "C" fn XInputSetState(dwuserindex: u32, pvibration: *const XINPUT_VIBRATION) -> u32 {
     xinput().XInputSetState(dwuserindex, pvibration)
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern fn XInputGetCapabilities(dwuserindex: u32, dwflags: XINPUT_FLAG, pcapabilities: *mut XINPUT_CAPABILITIES) -> u32 {
+pub extern "C" fn XInputGetCapabilities(dwuserindex: u32, dwflags: XINPUT_FLAG, pcapabilities: *mut XINPUT_CAPABILITIES) -> u32 {
     xinput().XInputGetCapabilities(dwuserindex, dwflags, pcapabilities)
 }
