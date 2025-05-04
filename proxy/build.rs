345e21ef0df8d90a266eab659c1cea0a07feeb54
XdotCore <xdotcoredev@gmail.com>
2025-05-04 11:55:04 -0500

    better os family checking

--- proxy/build.rs
+++ proxy/build.rs
@@ -4,9 +4,13 @@ fn main() {
     let lib_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let target_dir = format!("{}/../../..", env::var("OUT_DIR").unwrap()); // hacky but works
 
-    #[cfg(windows)]
-    {
-        println!("cargo::rustc-cdylib-link-arg=/DEF:{lib_dir}/proxy.def");
-        println!("cargo::rustc-cdylib-link-arg=/OUT:{target_dir}/XInput1_3.dll");
+    match std::env::var("CARGO_CFG_TARGET_FAMILY") {
+        Ok(family) => {
+            if family.contains("windows") {
+                println!("cargo::rustc-cdylib-link-arg=/DEF:{lib_dir}/proxy.def");
+                println!("cargo::rustc-cdylib-link-arg=/OUT:{target_dir}/XInput1_3.dll");
+            }
+        }
+        _ => {}
     }
 }
